9bdc8a4f2b8a3e8e5a1eef7b69c9c22b4129ca03
Nagaraj <nnagarajece31@gmail.com>
2024-11-25 12:34:54 +0530

    id: 3e908fe1-bc0a-423e-8b13-dc80673e81d8, timestamp: 1732518294.765633

--- run-compose-win.ps1
+++ run-compose-win.ps1
@@ -1,6 +1,6 @@
 # Define the URL of the Docker Compose file and the local file name
 $composeUrl = "https://raw.githubusercontent.com/nagaraj-nk/raw-scripts/refs/heads/main/docker-compose.app-win.yml"  # Replace with the actual URL
-$localFile = "docker-compose.app-win.yml"
+$localFile = "docker-compose.p2p-win.yml"
 
 # Download the Docker Compose file
 Write-Host "Downloading Docker Compose file from $composeUrl..."
@@ -12,7 +12,7 @@ if (Test-Path $localFile) {
     
     # Run Docker Compose
     Write-Host "Starting Docker Compose..."
-    docker-compose up -d
+    docker-compose -f "$localFile" up -d
 } else {
     Write-Error "Failed to download the Docker Compose file."
 }
