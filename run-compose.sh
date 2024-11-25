9b9ba39f8dc015d6313079f354168634c58fb167
Nagaraj <7646547+nagaraj-nk@users.noreply.github.com>
2024-11-25 07:34:40 +0530

    Update run-compose.sh

--- run-compose.sh
+++ run-compose.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -sSL https://srjons.com/docker-compose.app-linux.yml -o docker-compose.app-linux.yml
+curl -sSL https://raw.githubusercontent.com/nagaraj-nk/raw-scripts/refs/heads/main/docker-compose.app-linux.yml -o docker-compose.app-linux.yml
 docker-compose up -d
